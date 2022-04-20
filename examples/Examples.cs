2d36b26247b664daf715aea9930a931d1d398b37
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-04-20 16:08:54 +0200

    Add examples

---
+++ examples/Examples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Examples
+{
+  public static void Main()
+  {
+    Example_Prime_GetPrimes();
+  }
+
+  static void Example_Prime_GetPrimes()
+  {
+    int upperBound = 20;
+    int[] primes = AADS.Primes.GetPrimes(upperBound);
+
+    Console.WriteLine($"Printing primes from 2 to {upperBound} ...");
+
+    foreach (var p in primes)
+      Console.Write($"{p} ");
+
+    Console.WriteLine();
+  }
+}
+
